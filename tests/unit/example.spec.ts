e9c46ddfab92f76a4f70f5d517f758f89cbe5e3e
Joe Strouth <joestrouth1@gmail.com>
2021-03-24 16:54:01 -0500

    init

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,9 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Home from '@/views/Home.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('Home.vue', () => {
+  it('renders test text', () => {
+    const wrapper = shallowMount(Home)
+    expect(wrapper.text()).toMatch('Test')
   })
 })
