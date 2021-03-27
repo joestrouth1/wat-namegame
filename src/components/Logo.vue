e890089ef6e3f3c8101d324c41e44c0db0dee90f
Joe Strouth <joestrouth1@gmail.com>
2021-03-26 23:40:45 -0500

    feat: add logo, button components

---
+++ src/components/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+<img src="@/assets/logo/Variant=Splash.svg" alt="The Name Game" class="logo" />
+</template>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+  height: auto;
+}
+</style>
