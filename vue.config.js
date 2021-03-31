03e9ec4779522139060818f6ae12e10ff7a51937
Joe Strouth <joestrouth1@gmail.com>
2021-03-31 18:26:40 -0500

    build(pwa): update application icon(s)

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,14 @@ const pwa = {
   manifestOptions: {
     short_name: 'Name Game',
     start_url: '/'
+  },
+  iconPaths: {
+    faviconSVG: 'img/icons/favicon.svg',
+    favicon32: 'img/icons/favicon-32x32.png',
+    favicon16: 'img/icons/favicon-16x16.png',
+    appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+    maskIcon: 'img/icons/favicon.svg',
+    msTileImage: 'img/icons/msapplication-icon-144x144.png'
   }
 }
 
